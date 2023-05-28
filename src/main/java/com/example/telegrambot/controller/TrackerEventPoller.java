e421581a3cc2b4f78784aef5116373977fa93522
abylaikossay <abylai.kossay@gmail.com>
2023-05-29 03:21:48 +0600

    add telegram to pooling

--- src/main/java/com/example/telegrambot/controller/TrackerEventPoller.java
+++ src/main/java/com/example/telegrambot/controller/TrackerEventPoller.java
@@ -2,6 +2,7 @@ package com.example.telegrambot.controller;
 
 import com.example.telegrambot.models.TrackerEvent;
 import com.example.telegrambot.service.RiskManagementService;
+import com.example.telegrambot.service.TelegramBot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,17 +12,26 @@ import java.util.List;
 public class TrackerEventPoller {
 
     private final RiskManagementService riskManagementService;
+    private final TelegramBot telegramBot;
 
     @Autowired
-    public TrackerEventPoller(RiskManagementService riskManagementService) {
+    public TrackerEventPoller(RiskManagementService riskManagementService, TelegramBot telegramBot) {
         this.riskManagementService = riskManagementService;
+        this.telegramBot = telegramBot;
     }
 
     public void startPolling() {
         while (true) {
-            List<TrackerEvent> event = riskManagementService.getTrackerEvents();
-            // Process the received event
-            System.out.println(event);
+            List<TrackerEvent> eventList = riskManagementService.getTrackerEvents();
+            if (eventList.size() > 0) {
+                eventList.forEach(trackerEvent -> {
+                    if (trackerEvent.getRelativeDrawdown() < 3.0) {
+                        telegramBot.sendMessage(211178284, "Relative Drawdown" + trackerEvent.getRelativeDrawdown());
+                    }
+                });
+            } else {
+                telegramBot.sendMessage(211178284, "Пулинг запрос успешно получен");
+            }
         }
     }
 }
--- src/main/java/com/example/telegrambot/service/TelegramBot.java
+++ src/main/java/com/example/telegrambot/service/TelegramBot.java
@@ -40,19 +40,14 @@ public class TelegramBot extends TelegramLongPollingBot {
             comandRecieved(chatId, update.getMessage().getChat().getUserName());
         }
     }
-    private void comandRecieved(long chatId, String name) {
-        if (name.equals("assiyazhm")) {
-            sendMessage(chatId, "Привет Солнышко, я тебя люблю!");
-        } else if (name.equals("abylaikossay")) {
-            sendMessage(chatId, "Приветсвтую, мой король!");
-        } else {
-            sendMessage(chatId, "Hello," + name + ". How are you?");
-        }
 
+    private void comandRecieved(long chatId, String name) {
+        System.out.println("chatId:" + chatId);
+        sendMessage(chatId, "Hello," + name + ". How are you?");
 
     }
 
-//    private void getDataFromMetaApi() throws IOException, InterruptedException {
+    //    private void getDataFromMetaApi() throws IOException, InterruptedException {
 //        String apiUrl = "https://risk-management-api-v1.new-york.agiliumtrade.ai";
 //        String accountId = "6a29cf6c-ee73-4e71-963b-c9c5fd949055";
 //        String apiToken = "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJhMDkxZWU3NDcwZGYyMGQ3NDIxY2Y3MDAwODA2OTdkMyIsInBlcm1pc3Npb25zIjpbXSwiYWNjZXNzUnVsZXMiOlt7Im1ldGhvZHMiOlsicmlzay1tYW5hZ2VtZW50LWFwaTpyZXN0OnB1YmxpYzoqOioiXSwicm9sZXMiOlsicmVhZGVyIiwid3JpdGVyIl0sInJlc291cmNlcyI6WyIqOiRVU0VSX0lEJDo2YTI5Y2Y2Yy1lZTczLTRlNzEtOTYzYi1jOWM1ZmQ5NDkwNTUiXX1dLCJ0b2tlbklkIjoiMjAyMTAyMTMiLCJpbXBlcnNvbmF0ZWQiOmZhbHNlLCJyZWFsVXNlcklkIjoiYTA5MWVlNzQ3MGRmMjBkNzQyMWNmNzAwMDgwNjk3ZDMiLCJpYXQiOjE2ODUwMzAyOTh9.bZGHNa_Dr2G6x3_bPe0_dh6kQLfIP2R04-RmSamNBecoo2aeBIKiOngquVwQouRfqfkqVCf55QzOIMgAFcfftQ4RDLurKu2gktfA3eWrplwlgSFK3HOTfXCqROSFhWveHvS0s0AUbzQb0Tuqu5SHY8F-HdUnUzmMPo85ru7I1ibvCV42nejIxO1qKP3bqVNdkFQMqHyvZk7cAqjIyPVfAdcXBUCMeOGMvNeqRbyJ0enw0S52WE1sk-nARKmdyoRPVIURSinQY2VrZN0-K0R9WwR8AdIcMM211HgibRb4h4EMJjVUlah85uIv0wWqvzVqZo48dQ1Z0ByIBP88GlJ6U0nP__QiPJWUZqHRyzxCT6N27BYmUaTTu5k_F73EmldFQD5eqkQs2bFYcgaVMXTwpQb89K7FRhpSWTdSRPp_5JLyvT_ry-B2POIagDpgXNx_BdoU7EosaY6ziigLTzjP-ItzSc4lyjW7BMiV0Rgr28GwWHIg82UxiKrt-w-mNn-_-G8yTneixG3jK1CKijC3qtejB8nl_BWUijnd37gO4bcu1LK6brtMF3Q6s-DU3bie1UHbLHEhgIs61mmOEudXsnQAFiT5V88KATICJJLvowqtBTuM3vztx3qN5kl3aPWZY5v1B5TJ8NXczOA5kM7zCZM40GMNro7E-PTnGBz6OeU";
@@ -74,7 +69,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 //            System.out.println("Error: " + response.statusCode());
 //        }
 //    }
-    private void sendMessage(long chatId, String textToSend) {
+    public void sendMessage(long chatId, String textToSend) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
